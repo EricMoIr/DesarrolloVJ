47beb470e4e7d95b5288b131d2440d82ff7fc0ee
Eric Mordezki <eric_mordezki@gmail.com>
2017-12-11 18:19:03 -0300

    Agregue las estrellitas al darse contra un misil

--- AnimalRace/Assets/Standard Assets/Vehicles/Car/Scripts/MissileCollisionStatus.cs
+++ AnimalRace/Assets/Standard Assets/Vehicles/Car/Scripts/MissileCollisionStatus.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Assets.Scripts.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 public class MissileCollisionStatus : AbnormalStatus
 {
     private const float STOP_MULTIPLIER = 0.001f;
     private const double STOP_DURATION = 5;
     private double remainingDuration;
+    private GameObject stars;
     public double Duration
     {
         get
@@ -21,16 +24,28 @@ public class MissileCollisionStatus : AbnormalStatus
         remainingDuration = STOP_DURATION;
         car.AddAbnormalStatus(this);
         car.ChangeMaxSpeed(STOP_MULTIPLIER);
+        ShowStars(car);
     }
 
     public void Deactivate(CarBehavior car)
     {
         car.RemoveAbnormalStatus(this);
         car.ChangeMaxSpeed(1 / STOP_MULTIPLIER);
+        stars.SetActive(false);
     }
 
     public void ReduceTime(float deltaTime)
     {
         remainingDuration -= deltaTime;
+    }
+
+    private void ShowStars(CarBehavior car)
+    {
+        stars =
+            ObjectPooler.GetInstance().GetInactiveObjectOfType("StunnedStatePrefab");
+        Vector3 starsPosition = car.transform.position;
+        starsPosition.y += car.GetComponent<BoxCollider>().size.y;
+        stars.transform.position = starsPosition;
+        stars.SetActive(true);
     }
 }
